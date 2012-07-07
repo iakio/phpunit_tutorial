74dd902caad91cef192e72a4f4fd77f11f237c29
ISHIDA Akio <ishida@coderium.com>
2012-07-07 16:16:38 +0900

    乗算のテストを実装

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,8 +1,12 @@
 <?php
+require_once __DIR__ . '/../src/Calculator.php';
 class CalculatorTest extends PHPUnit_Framework_TestCase
 {
 	/** @test */
 	function multiplyで乗算結果が取得できる() {
-		$this->fail("まだ実装されていません");
+		$calc = new Calculator();
+		$expected = 12;
+		$actual = $calc->multiply(3, 4);
+		$this->assertThat($actual, $this->equalTo($expected));
 	}
 }
